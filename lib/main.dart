acc6f0458c14a7345ee7e19867920f3c72140f8c
Javier Andres Pedrozo Payares <pedrozopayares@gmail.com>
2020-11-08 22:31:18 -0500

    Challenge finishedgit status

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -33,15 +34,20 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
         child: FlatButton(
             onPressed: () {
-              print('I got clicked');
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+                print('ballNumber = $ballNumber');
+              });
             },
-            child: Image.asset('images/ball1.png')),
+            child: Image.asset('images/ball$ballNumber.png')),
       ),
     );
   }
