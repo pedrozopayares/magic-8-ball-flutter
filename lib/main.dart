89f142f8696388d8e825e11083f97b163087b619
Javier Andres Pedrozo Payares <pedrozopayares@gmail.com>
2020-11-08 21:50:55 -0500

    First commit.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Magic8Ball(),
       ),
     );
+
+class Magic8Ball extends StatefulWidget {
+  Magic8Ball({Key key}) : super(key: key);
+
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Hello, World!'),
+    );
+  }
+}
